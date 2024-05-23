c52c7ca4a31f12b7bc373218f510de64298a3ea6
Ohad Manor <ohad.manor@gmail.com>
2024-05-23 14:40:13 +0300

    delete unnecessary old files

--- ENV_Install.bat
+++ ENV_Install.bat
@@ -3,9 +3,6 @@ echo Install 3D tiles Enviroment
 
 set Directory=%~dp0
 
-cmd /c npm install %Directory%3DTools\3d-tiles-tools\ -g 3d-tiles-tools
-cmd /c npm install %Directory%3DTools\obj23dtiles\ -g obj23dtiles
-
 set BlenderFolder=%Directory%\blender
 call %BlenderFolder%\blender startup.blend --python  setup.py
 
